4587cb567bae6972d86dfa8cee849218d8d49198
Jayeshrakhonde07 <jayeshrakhonde05@gmail.com>
2026-09-22 15:10:28 +0530

    intial commit

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+    const navigations = [
+    {name:"Home",path:'#home'},
+    {name:"About",path:'#about'},
+    {name:"Skills",path:'#skills'},
+    {name:"Projects",path:'#projects'},
+    {name:"Certifications",path:'#certifications'},
+    {name:"Contact",path:'#contact'},
+
+]
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
